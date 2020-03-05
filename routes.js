37665615b131c73c7f2ddcb99f16570ec69f39f3
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2020-03-05 13:43:44 +0100

    regisztrációs végpont

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+const fs = require('fs');
 
 router.route('/login').post((req, res) => {
     if(req.body.username && req.body.password) {
@@ -28,6 +29,22 @@ router.route('/logout').post((req, res) => {
     } else {
         res.status(403).send("Jelentkezz előbb be, hogy kijelentkezhess")
     }
+});
+
+router.route('/register').post((req, res) => {
+    if(req.body.username && req.body.password) {
+        const users = require('./users.json');
+        if(!users.users[req.body.username]) {
+            users.users[req.body.username] = {
+                password: req.body.password,
+                role: 'user'
+            }
+            fs.writeFileSync('./users.json', JSON.stringify(users));
+            return res.status(200).send("Regisztráció sikeres");
+        }
+        return res.status(400).send("Már van ilyen felhasználónév!");
+    }
+    return res.status(400).send("Hiányzik a felhasználónév vagy a jelszó");
 });
 
 module.exports = router;
